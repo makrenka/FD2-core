20fff95b4b922e2c6569c57e19c36eef1a9db20e
makrenka <smokrenko79@gmail.com>
2022-11-08 14:29:06 +0300

    add plus minus for Button

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -9,7 +9,34 @@ export class Button extends Component {
         }
     }
 
-    
+    plus() {
+        this.setState((state) => {
+            return {
+                ...state,
+                count: state.count < 10 ? state.count += 1 : state.count,
+            }
+        });
+    }
+
+    minus() {
+        this.setState((state) => {
+            return {
+                ...state,
+                count: state.count > 0 ? state.count -= 1 : state.count,
+            }
+        });
+    }
+
+    registerEvents() {
+        this.addEventListener('click', (evt) => {
+            if (evt.target.closest('.plus')) {
+                this.plus();
+            }
+            if (evt.target.closest('.minus')) {
+                this.minus();
+            }
+        })
+    }
 
     render() {
         return `
